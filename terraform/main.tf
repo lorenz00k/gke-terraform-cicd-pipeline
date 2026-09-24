f9d2c08c9cb4e7a7e8d26ca87ef595b9d60107ab
Lorenz <lorenz.klemm@gmail.com>
2026-09-24 16:49:19 +0200

    Add Artifact Registry repository to Terraform config

--- terraform/main.tf
+++ terraform/main.tf
@@ -45,3 +45,9 @@ resource "google_container_node_pool" "primary_nodes" {
   }
 }
 
+resource "google_artifact_registry_repository" "repo" {
+  location      = var.region
+  repository_id = "gke-demo-repo"
+  format        = "DOCKER"
+}
+
